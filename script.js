978ff2b29eaafd3c74468c28b83f80a5172491fb
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-07-20 17:35:40 -0400

    Fetching Data success Rendering to Dom

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const adviceId = document.getElementById('advice-id');
+const adviceText = document.querySelector('.advice-text');
+console.log(adviceId);
+console.log(adviceText);
+
+const displayAdvice = (data) => {
+  adviceText.innerHTML = data.slip.advice;
+  adviceId.innerHTML = data.slip.id;
+};
+
+fetch('https://api.adviceslip.com/advice')
+  .then((response) => response.json())
+  .then(displayAdvice);
