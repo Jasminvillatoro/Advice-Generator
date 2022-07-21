86fe6bda334f16c235fac3defafe019b6d1d5726
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-07-21 11:04:23 -0400

    DOM and EventListeners Functionality for data

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const adviceId = document.getElementById('advice-id');
 const adviceText = document.querySelector('.advice-text');
-console.log(adviceId);
-console.log(adviceText);
+const button = document.querySelector('.button');
 
 const displayAdvice = (data) => {
-  adviceText.innerHTML = data.slip.advice;
-  adviceId.innerHTML = data.slip.id;
+  adviceText.innerHTML = `"${data.slip.advice}"`;
+  adviceId.innerHTML = 'Advice # ' + data.slip.id;
 };
 
-fetch('https://api.adviceslip.com/advice')
-  .then((response) => response.json())
-  .then(displayAdvice);
+const getAdvice = () => {
+  fetch('https://api.adviceslip.com/advice')
+    .then((response) => response.json())
+    .then(displayAdvice);
+};
+
+button.addEventListener('click', getAdvice);
+window.onload = () => {
+  getAdvice();
+};
